debbc7ff595d761f39327eb238a9fc43caa48dd0
jared-one <cizeforthemize@gmail.com>
2025-06-02 22:29:27 +0200

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,1 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # You can send any data if needed, like name or messages
+    return render_template('index.html', name="Rati")
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
