ab8a63b2924ffdd2c06377af34c2a89c512827ba
jared-one <cizeforthemize@gmail.com>
2025-06-03 06:45:26 +0200

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 import os
 
-app = Flask(__name__, template_folder=os.path.abspath('../templates'), static_folder=os.path.abspath('../static'))
+app = Flask(__name__,
+            template_folder=os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates')),
+            static_folder=os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'static')))
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# No need for if __name__ == '__main__': app.run() for Vercel
